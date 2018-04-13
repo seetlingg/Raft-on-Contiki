b67d076284608350b08975584953f30d5adda14f
JTEnglish <je787413@ohio.edu>
2018-04-13 18:14:00 -0400

    starting on messages for leader election

--- src/raft.h
+++ src/raft.h
@@ -11,12 +11,37 @@
 #define TOTAL_NODES 4 //total number of nodes in network
 
 enum states {follower, candidate, leader};
+enum msg_types {heartbeat, election, vote};
 
 struct Raft {
   uint32_t term;
   uip_ipaddr_t voted_for;
   uint8_t timeout;
   enum states state;
+};
+
+// default message struct to determine type and term on incoming messages
+struct Msg {
+  enum msg_types type;
+  uint32_t term;
+};
+
+// heartbeat message
+struct Heartbeat {
+  enum msg_types type;
+  uint32_t term;
+};
+
+// start election message
+struct Election {
+  enum msg_types type;
+  uint32_t term;
+};
+
+// vote response for election
+struct Vote {
+  enum msg_types type;
+  uint32_t term;
 };
 
 void raft_init(struct Raft *node);
