38cec7c0c36342d02cc850433fb6556c43eb77ac
seetlingg <66860317+seetlingg@users.noreply.github.com>
2022-01-13 16:59:07 +0800

    Update project-conf.h

--- src/project-conf.h
+++ src/project-conf.h
@@ -1,6 +1,20 @@
+/* ------ Network Config ------ */
+
+// #undef NETSTACK_NETWORK
+// #define NETSTACK_NETWORK rime_driver
+
+#undef NETSTACK_CONF_MAC 
+#define NETSTACK_CONF_MAC  csma_driver  //csma_driver //nullmac_driver // csma_driver
+
+#undef NETSTACK_CONF_RDC
+#define NETSTACK_CONF_RDC  nullrdc_driver //nullrdc_driver // contikimac_driver // cxmac_driver  
+
+#undef NETSTACK_CONF_FRAMER
+#define NETSTACK_CONF_FRAMER framer_802154 // framer_nullmac
+
 #undef NETSTACK_CONF_RADIO
 #define NETSTACK_CONF_RADIO cc2420_driver
-#undef NETSTACK_CONF_FRAMER
-#define NETSTACK_CONF_FRAMER framer_802154
-#undef NETSTACK_CONF_MAC
-#define NETSTACK_CONF_MAC  csma_driver
+
+
+// #undef TIMESYNCH_CONF_ENABLED
+// #define TIMESYNCH_CONF_ENABLED 1
